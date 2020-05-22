5e198a6d41e11034900ebf92444cfec334c07fb9
Vladimir Ciobanu <vladimir-gabriel.ciobanu@ing.com>
2020-05-22 18:36:28 +0300

    forgot to update shell.nix

--- src/main/g8/shell.nix
+++ src/main/g8/shell.nix
@@ -4,5 +4,5 @@ let inherit (import ./default.nix) pkgs;
 in pkgs.mkShell {
   # Add dependencies here as they are needed.
   nativeBuildInputs = with pkgs; [ jdk sbt metals ];
-  NIX_PATH = "nixkpgs=${pkgs.path}";
+  NIX_PATH = "nixkpgs=\${pkgs.path}";
 }
